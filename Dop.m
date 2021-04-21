0de03c3a617823f91a22921faa5244e1feca45c5
Shilionz <50146869+Shilionz@users.noreply.github.com>
2021-04-21 23:47:23 +0800

    Update Dop.m

--- Dop.m
+++ Dop.m
@@ -1,7 +1,7 @@
 (* ::Package:: *)
 
-Dop[a_,b_,t_,x_,m_,n_]:=If[n>0,
-Dop[D[a,x],b,x,t,n-1,m]-Dop[a,D[b,x],x,t,n-1,m],
-If[m>0,
-Dop[D[a,t],b,t,x,m-1,n]-Dop[a,D[b,t],t,x,m-1,n],
-a*b]];
+Dop[a_, b_, x_, t_, m_, n_] := If[n > 0,
+Dop[D[a, t], b, t, x, n - 1, m] - Dop[a, D[b, t], t, x, n - 1, m],
+If[m > 0,
+Dop[D[a, x], b, x, t, m - 1, n] - Dop[a, D[b, x], x, t, m - 1, n],
+a*b]]
